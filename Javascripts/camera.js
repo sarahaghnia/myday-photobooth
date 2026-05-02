3e13b8f70f079fff73a0f8f172eb37ddc53fca71
sarahaghnia <sarahaghniahusna@gmail.com>
2026-05-02 20:20:57 +0700

    Update camera.js

--- Javascripts/camera.js
+++ Javascripts/camera.js
@@ -18,7 +18,7 @@ let photoStage = 0; // 0=top,1=middle,2=bottom,3=done
 const moveVideoToHalf = i => {
   const { video } = elements;
   video.style.display = 'block';
-  video.style.top = i === 0 ? '0' : i === 1? '33.33%' : '6666%'
+  video.style.top = i === 0 ? '0' : i === 1? '33.33%' : '6666%';
   video.style.left = '0';
   video.style.width = '100%';
   video.style.height = '33.33%';
